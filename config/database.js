45a8f2c637bd64639930b2de626614458c52468b
ivs98 <ivs.fotos1@gmail.com>
2024-09-13 15:12:26 +0200

    insert posts into database

--- config/database.js
+++ config/database.js
@@ -1,14 +1,24 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { mysql2 } from 'mysql2/promise';
+import mysql2 from 'mysql2/promise';
 
-const connection = await mysql2.createConnection({
+const connection = mysql2.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
 });
 
-export default connection;
-export { mysql2 };
+const connectDB = async () => {
+    try {
+        const conn = await connection;
+        console.log('Conexión establecida a la base de datos')
+        return conn;
+    } catch (error) {
+        console.log('Error al connectarte a la base de datos')
+        throw error;
+    }
+}
+
+export default connectDB;
--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import connectDB from '../config/database.js';
 
 const router = Router();
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const { title, content, category, tags } = req.body;
 
     console.log(title, content, category, tags);
@@ -17,47 +18,69 @@ router.post('/', (req, res) => {
         return res.status(400).send("Los datos son incorrectos, revísalos");
     }
 
+    let connection;
     //insertar en la base de datos
     try {
-        const [result] = connection.execute(
-            'INSERT INTO post (title, content, category, createdAt) VALUES (?, ?, ?, ?, ?)',
-            [title, content, category, now()]
+        connection = await connectDB();
+
+        await connection.beginTransaction();
+
+        const [result] = await connection.execute( //query vs execute, buscar diferencias
+            'INSERT INTO post (title, content, category) VALUES (?, ?, ?)',
+            [title, content, category]
         );
 
         const postId = result.insertId;
 
         //si es correcto, me devuelve el id, lo cojo y hago un select para devolver el post entero
         if (postId){
-            const [rows] = connection.execute('SELECT * FROM post WHERE id = ?', [postId]);
+            const [rows] = await connection.execute('SELECT * FROM post WHERE id = ?', [postId]);
             //return res.status(201).json(rows[0]);
             for (const tag of tags) {
-                const [result] = connection.execute(
+                const [result] = await connection.execute(
                     'SELECT id FROM tag WHERE tag = ?', [tag]);
                 
                 const tagId = result.insertId;
 
                 if (tagId) {
-                    const [result2] = connection.execute(
-                        'INSERT INTO post_tag (postId, tagId) VALUES (?, ?)',
+                    const [result2] = await connection.execute(
+                        'INSERT INTO post_tag (id_post, id_tag) VALUES (?, ?)',
                         [postId, tagId]
                     );
                 }else{
-                    const [result3] = connection.execute(
+                    const [result3] = await connection.execute(
                         'INSERT INTO tag (tag) VALUES (?)',
                         [tag]
                     );
                     const tagId = result3.insertId;
-                    const [result4] = connection.execute(
-                        'INSERT INTO post_tag (postId, tagId) VALUES (?, ?)',
+                    const [result4] = await connection.execute(
+                        'INSERT INTO post_tag (id_post, id_tag) VALUES (?, ?)',
                         [postId, tagId]
                     );
                 }
             }
+            
+            await connection.commit();
+
+            //Select para obtener los datos del post introducido
+            // Seleccionar el post recién creado
+            const [rows2] = await connection.execute('SELECT * FROM post WHERE post.id = ?', [postId]);
+            /* const [tags] = await connection.execute(
+                'SELECT tag.tag FROM tag INNER JOIN post_tag ON tag.id = post_tag.id_tag WHERE post_tag.id_post = ?', [postId]
+            );
+            rows2[0].tags = tags; */
+            console.log(rows2[0]);
+            
+            return res.status(201).json(rows2[0]); //faltan los tags
+
+
         }
         
-    } catch (error) {
+        
+    } catch (error){
+        await connection.rollback();
         console.error('Error al insertar en la base de datos:', error);
-        throw error;
+        return res.status(400).send("Bad Request");
     }
 
 
